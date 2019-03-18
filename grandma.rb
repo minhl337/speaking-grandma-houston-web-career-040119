8ce85ba8e546ba8e19f169593d0cc6f15aeb30f1
minh nguyen <imnotminh@gmail.com>
2019-03-18 00:11:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(string)
-  if string===string.upcase
+def speak_to_grandma(gma)
+  if gma===gma.upcase
     return 
 end
